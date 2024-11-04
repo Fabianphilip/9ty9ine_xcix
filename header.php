69e0cec35f1b0a93432dbcfc6463cd137d9db075
MacBook Pro <igwemorgansomto@gmail.com>
2024-11-04 10:54:12 +0100

    commit

--- header.php
+++ header.php
@@ -225,11 +225,11 @@
                 <div class="">
                     <div class="row">
                         <!-- LOGO AREA START -->
-                        <div class="col-4 col-lg-2 col-md-6">
+                        <div class="col-4 col-lg-2 col-md-6" style="align-content: center;">
                             <div class="logo m-0">
                                 <a href="/">
                                     <!-- <img src="assets/img/9ty9inelogo.png" alt="Logo" style="width: 50px;"> -->
-                                    <strong><h1 style="padding: 20px; color: black; font-family: initial; font-weight: 900;">9ty9ine</h1></strong>
+                                    <strong><h1 style="padding: 20px; color: black; font-family: initial; font-weight: 900; margin: 0px;">9ty9ine</h1></strong>
                                 </a>
                             </div>
                         </div>
