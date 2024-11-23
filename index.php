b17b689b8375f4c4301424d8f83b18a0686eec56
MacBook Pro <igwemorgansomto@gmail.com>
2024-11-23 01:26:03 +0100

    commit

--- index.php
+++ index.php
@@ -134,6 +134,24 @@
                 </div>
                 <img src="assets/img/6729bf40a3bc44234.png" alt="Model Image" class="model mobile-image">
             </div>
+
+            <div class="slide" style="background-color: #5d5c81;">
+                <div class="content">
+                    <h1 style="color: white !important; font-size: 28px; line-height: 1;"><strong>XCIX</strong></h1>
+                    <p style="color: white !important; font-size: 22px;" >Compliment Dresses</p>
+                    <div style="margin-top: 15px;"><a href="shop" style="background: black; color: white; padding: 10px; margin-top: 20px;">Shop Now</a></div>
+                </div>
+                <img src="assets/img/PHOTO-2024-11-20-14-13-10_2.png" alt="Model Image" class="model mobile-image">
+            </div>
+
+            <div class="slide" style="background-color: #82bcb0;">
+                <div class="content">
+                    <h1 style="color: white !important; font-size: 28px; line-height: 1;"><strong>XCIX</strong></h1>
+                    <p style="color: white !important; font-size: 22px;" >Compliment Dresses</p>
+                    <div style="margin-top: 15px;"><a href="shop" style="background: black; color: white; padding: 10px; margin-top: 20px;">Shop Now</a></div>
+                </div>
+                <img src="assets/img/PHOTO-2024-11-20-14-13-10.png" alt="Model Image" class="model mobile-image">
+            </div>
               
         </div>
 
@@ -153,6 +171,28 @@
                     <div class="background_mobile"></div>
                     <div class="content_mobile">
                         <img src="assets/img/6729bf40a3bc44234.png" alt="Model 2" class="model-image_mobile">
+                        <div class="overlay_mobile"></div>
+                        <h1 style="color: white !important; font-size: 38px; line-height: 1; z-index: 9;"><strong>XCIX</strong></h1>
+                        <p style="color: white !important; font-size: 22px; z-index: 9;">Compliment Dresses</p>
+                        <div style="z-index: 8; margin-top: 15px;"><a href="shop" style="background: black; color: white; padding: 10px; z-index: 9; font-size: 20px; margin-top: 10px;">Shop Now</a></div>
+                    </div>
+                </div>
+
+                <div class="slide_mobile" style="background-color: #5d5c81;">
+                    <div class="background_mobile"></div>
+                    <div class="content_mobile">
+                        <img src="assets/img/PHOTO-2024-11-20-14-13-10_2.png" alt="Model 2" class="model-image_mobile">
+                        <div class="overlay_mobile"></div>
+                        <h1 style="color: white !important; font-size: 38px; line-height: 1; z-index: 9;"><strong>XCIX</strong></h1>
+                        <p style="color: white !important; font-size: 22px; z-index: 9;">Compliment Dresses</p>
+                        <div style="z-index: 8; margin-top: 15px;"><a href="shop" style="background: black; color: white; padding: 10px; z-index: 9; font-size: 20px; margin-top: 10px;">Shop Now</a></div>
+                    </div>
+                </div>
+
+                <div class="slide_mobile" style="background-color: #82bcb0;">
+                    <div class="background_mobile"></div>
+                    <div class="content_mobile">
+                        <img src="assets/img/PHOTO-2024-11-20-14-13-10.png" alt="Model 2" class="model-image_mobile">
                         <div class="overlay_mobile"></div>
                         <h1 style="color: white !important; font-size: 38px; line-height: 1; z-index: 9;"><strong>XCIX</strong></h1>
                         <p style="color: white !important; font-size: 22px; z-index: 9;">Compliment Dresses</p>
