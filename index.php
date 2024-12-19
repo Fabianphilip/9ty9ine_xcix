5cabfe979fae6e0f499b4e05472c66486e69a876
MacBook Pro <igwemorgansomto@gmail.com>
2024-12-19 22:27:55 +0100

    commit

--- index.php
+++ index.php
@@ -117,7 +117,7 @@
 
         <div id="breadcrumb" class="breadcrum_mobile">
             
-            <div class="slide" style="background-color: #555257;">
+            <div class="slide" style="background-color: #fff;">
                 <div class="content">
                     <h1 style="color: white !important; font-size: 28px; line-height: 1;"><strong>XCIX</strong></h1>
                     <p style="color: white !important; font-size: 22px;">Luxury Street Wear redefined</p>
@@ -144,7 +144,7 @@
                 <img src="assets/img/PHOTO-2024-11-20-14-13-10_2.png" alt="Model Image" class="model mobile-image">
             </div>
 
-            <div class="slide" style="background-color: #82bcb0;">
+            <div class="slide" style="background-color: #fff;">
                 <div class="content">
                     <h1 style="color: white !important; font-size: 28px; line-height: 1;"><strong>XCIX</strong></h1>
                     <p style="color: white !important; font-size: 22px;" >Compliment Fashion And Lifestyle</p>
@@ -157,13 +157,13 @@
 
         <div class="slider_mobile breadcrum_desktop">
             <div class="slider-container_mobile">
-                <div class="slide_mobile" style="background-color: #555257;">
+                <div class="slide_mobile" style="background-color: #fff;">
                     <div class="background_mobile"></div>
                     <div class="content_mobile">
                         <img src="assets/img/PHOT4874.png" alt="Model 1" class="model-image_mobile">
                         <div class="overlay_mobile"></div>
-                        <h1 style="color: white !important; font-size: 38px; line-height: 1; z-index: 9;"><strong>XCIX</strong></h1>
-                        <p style="color: white !important; font-size: 22px; z-index: 9;">Luxury Street Wear redefined</p>
+                        <h1 style="color: black !important; font-size: 38px; line-height: 1; z-index: 9;"><strong>XCIX</strong></h1>
+                        <p style="color: black !important; font-size: 22px; z-index: 9;">Luxury Street Wear redefined</p>
                         <div style="z-index: 8; margin-top: 15px;"><a href="shop" style="background: black; color: white; padding: 10px; z-index: 9; font-size: 20px; margin-top: 10px;">Shop Now</a></div>
                     </div>
                 </div>
@@ -188,13 +188,13 @@
                     </div>
                 </div>
 
-                <div class="slide_mobile" style="background-color: #82bcb0;">
+                <div class="slide_mobile" style="background-color: #fff;">
                     <div class="background_mobile"></div>
                     <div class="content_mobile">
                         <img src="assets/img/PHOTO6767.png" alt="Model 2" class="model-image_mobile">
                         <div class="overlay_mobile"></div>
-                        <h1 style="color: white !important; font-size: 38px; line-height: 1; z-index: 9;"><strong>XCIX</strong></h1>
-                        <p style="color: white !important; font-size: 22px; z-index: 9;">Compliment Fashion AND Lifestyle</p>
+                        <h1 style="color: black !important; font-size: 38px; line-height: 1; z-index: 9;"><strong>XCIX</strong></h1>
+                        <p style="color: black !important; font-size: 22px; z-index: 9;">Compliment Fashion AND Lifestyle</p>
                         <div style="z-index: 8; margin-top: 15px;"><a href="shop" style="background: black; color: white; padding: 10px; z-index: 9; font-size: 20px; margin-top: 10px;">Shop Now</a></div>
                     </div>
                 </div>
